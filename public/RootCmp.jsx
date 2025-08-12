fea3f6352405779e0b6db6563557277172931fe7
Lioz Fired <friedlioz@gmail.com>
2025-08-12 17:33:28 +0300

    Update RootCmp to supporting auth and users

--- public/RootCmp.jsx
+++ public/RootCmp.jsx
@@ -1,5 +1,8 @@
+const { useState } = React
 const Router = ReactRouterDOM.BrowserRouter
 const { Route, Routes } = ReactRouterDOM
+
+import { authService } from './services/auth.service.js'
 
 import { UserMsg } from './cmps/UserMsg.jsx'
 import { AppHeader } from './cmps/AppHeader.jsx'
@@ -7,25 +10,34 @@ import { AppFooter } from './cmps/AppFooter.jsx'
 import { Home } from './pages/Home.jsx'
 import { BugIndex } from './pages/BugIndex.jsx'
 import { BugDetails } from './pages/BugDetails.jsx'
+import { UserDetails } from './pages/UserDetails.jsx'
+import { LoginSignup } from './pages/LoginSignup.jsx'
 import { AboutUs } from './pages/AboutUs.jsx'
 import { NotFound } from './cmps/NotFound.jsx'
 
 export function App() {
-    return <Router>
-        <div className="app-wrapper">
-            <UserMsg />
-            <AppHeader />
-            <main className="container">
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/bug" element={<BugIndex />} />
-                    <Route path="/bug/:bugId" element={<BugDetails />} />
-                    <Route path="/about" element={<AboutUs />} />
+    const [loggedinUser, setLoggedinUser] = useState(authService.getLoggedinUser())
 
-                    <Route path='*' element={<NotFound />} />
-                </Routes>
-            </main>
-            <AppFooter />
-        </div>
-    </Router>
+    return (
+        <Router>
+            <div className="app-wrapper">
+                <UserMsg />
+                <AppHeader loggedinUser={loggedinUser} setLoggedinUser={setLoggedinUser} />
+
+                <main className="container">
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/bug" element={<BugIndex />} />
+                        <Route path="/bug/:bugId" element={<BugDetails />} />
+                        <Route path="/about" element={<AboutUs />} />
+
+                        <Route path="/auth" element={<LoginSignup setLoggedinUser={setLoggedinUser} />} />
+                        <Route path="/user/:userId" element={<UserDetails />} />
+                        <Route path='*' element={<NotFound />} />
+                    </Routes>
+                </main>
+                <AppFooter />
+            </div>
+        </Router>
+    )
 }
