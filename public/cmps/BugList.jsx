27f56683dbc8ad546fb50b4b0ee18f8f7f8249ae
Lioz Fired <friedlioz@gmail.com>
2025-08-12 20:02:50 +0300

    Update

--- public/cmps/BugList.jsx
+++ public/cmps/BugList.jsx
@@ -1,20 +1,32 @@
 const { Link } = ReactRouterDOM
 
+import { authService } from '../services/auth.service.js'
 import { BugPreview } from './BugPreview.jsx'
 
 export function BugList({ bugs, onRemoveBug, onEditBug }) {
 
+    const loggedinUser = authService.getLoggedinUser()
+
+    function isCreator(bug) {
+        if (!loggedinUser) return false
+        if (!bug.creator) return true
+        return loggedinUser.isAdmin || bug.creator._id === loggedinUser._id
+    }
+
     if (!bugs) return <div>Loading...</div>
-    return <ul className="bug-list">
-        {bugs.map(bug => (
-            <li key={bug._id}>
-                <BugPreview bug={bug} />
-                <section className="actions">
-                    <button><Link to={`/bug/${bug._id}`}>Details</Link></button>
-                    <button onClick={() => onEditBug(bug)}>Edit</button>
-                    <button onClick={() => onRemoveBug(bug._id)}>x</button>
-                </section>
-            </li>
-        ))}
-    </ul >
+
+    return (
+        <ul className="bug-list">
+            {bugs.map(bug => (
+                <li key={bug._id}>
+                    <BugPreview bug={bug} />
+                    <section className="actions">
+                        <button><Link to={`/bug/${bug._id}`}>Details</Link></button>
+                        <button onClick={() => onEditBug(bug)}>Edit</button>
+                        <button onClick={() => onRemoveBug(bug._id)}>x</button>
+                    </section>
+                </li>
+            ))}
+        </ul >
+    )
 }
--- public/pages/UserDetails.jsx
+++ public/pages/UserDetails.jsx
@@ -1,16 +1,22 @@
 const { useState, useEffect } = React
 const { useParams, useNavigate, Link } = ReactRouterDOM
 
+import { BugList } from "../cmps/BugList.jsx"
+import { bugService } from "../services/bug.service.remote.js"
 import { userService } from "../services/user.service.js"
+import { showErrorMsg, showSuccessMsg } from "../services/event-bus.service.js"
 
 export function UserDetails() {
 
     const [user, setUser] = useState(null)
+    const [userBugs, setUserBugs] = useState([])
+
     const params = useParams()
     const navigate = useNavigate()
 
     useEffect(() => {
         loadUser()
+        loadUserBugs()
     }, [params.userId])
 
     function loadUser() {
@@ -21,15 +27,56 @@ export function UserDetails() {
             })
     }
 
+    function loadUserBugs() {
+        bugService.query({ userId: params.userId })
+            .then(res => {
+                setUserBugs(res.bugs)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+
+    function onRemoveBug(bugId) {
+        bugService.remove(bugId)
+            .then(() => {
+                setUserBugs(prevBugs => prevBugs.filter(bug => bug._id !== bugId))
+                showSuccessMsg('Bug removed')
+            })
+            .catch(err => {
+                console.log('from remove bug', err)
+                showErrorMsg('Cannot remove bug')
+            })
+    }
+
+    function onEditBug(bug) {
+        const severity = +prompt('New severity?')
+        if (!severity) return alert('Please enter a severity')
+        const bugToSave = { ...bug, severity }
+        bugService.save(bugToSave)
+            .then(savedBug => {
+                console.log('Updated Bug:', savedBug)
+                setUserBugs(prevBugs =>
+                    prevBugs.map(currBug =>
+                        currBug._id === savedBug._id ? savedBug : currBug
+                    )
+                )
+                showSuccessMsg('Bug updated')
+            })
+            .catch(err => {
+                console.log('from edit bug', err)
+                showErrorMsg('Cannot update bug')
+            })
+    }
+
     if (!user) return <div>Loading...</div>
 
     return (
         <section className="user-details">
             <h1>User {user.fullname}</h1>
-            <pre>
-                {JSON.stringify(user, null, 2)}
-            </pre>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse hic saepe nihil magnam nostrum consectetur explicabo. Laboriosam unde eaque, animi repellat recusandae natus sed voluptatem porro, facilis obcaecati earum atque.</p>
+            {!userBugs || (!userBugs.length && <h2>No bugs to show</h2>)}
+            {userBugs && userBugs.length > 0 && <h3>Manage your bugs</h3>}
+            <BugList bugs={userBugs} onRemoveBug={onRemoveBug} onEditBug={onEditBug} />
             <Link to="/">Back Home</Link>
         </section>
     )
