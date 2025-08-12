43ad619cdaf81ef9e29488d0ab81ab5417d1235f
Lioz Fired <friedlioz@gmail.com>
2025-08-12 18:36:53 +0300

    Create LoginSignup.jsx cmp

---
+++ public/pages/LoginSignup.jsx
@@ -0,0 +1,88 @@
+const { useState } = React
+const { useNavigate } = ReactRouter
+
+import { showErrorMsg, showSuccessMsg } from "../services/event-bus.service.js"
+import { userService } from '../services/user.service.js'
+import { authService } from '../services/auth.service.js'
+
+export function LoginSignup({ setLoggedinUser }) {
+
+    const [isSignup, setIsSignup] = useState(false)
+    const [credentials, setCredentials] = useState(userService.getEmptyCredentials())
+    const navigate = useNavigate()
+
+    function handleChange({ target }) {
+        const { name: field, value } = target
+        setCredentials(prevCreds => ({ ...prevCreds, [field]: value }))
+    }
+
+    function handleSubmit(ev) {
+        ev.preventDefault()
+        isSignup ? signup(credentials) : login(credentials)
+    }
+
+    function login(credentials) {
+        authService.login(credentials)
+            .then(user => {
+                setLoggedinUser(user)
+                showSuccessMsg('Loggedin successfully')
+                navigate('/bug')
+            })
+            .catch(err => {
+                showErrorMsg('Couldnt login')
+            })
+    }
+
+    function signup(credentials) {
+        authService.signup(credentials)
+            .then(user => {
+                setLoggedinUser(user)
+                showSuccessMsg('Signed in successfully')
+                navigate('/bug')
+            })
+            .catch(err => {
+                showErrorMsg('Couldnt signup')
+            })
+    }
+
+    return (
+        <div className="login-page">
+            <form className="login-form" onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    name="username"
+                    value={credentials.username}
+                    placeholder="Username"
+                    onChange={handleChange}
+                    required
+                    autoFocus
+                />
+
+                <input
+                    type="password"
+                    name="password"
+                    value={credentials.password}
+                    placeholder="Password"
+                    onChange={handleChange}
+                    required
+                    autoComplete="off"
+                />
+
+                {isSignup && <input
+                    type="text"
+                    name="fullname"
+                    value={credentials.fullname}
+                    placeholder="Fullname"
+                    onChange={handleChange}
+                    required
+                />}
+            </form>
+
+            <div className="btns">
+                <a href="#" onClick={() => setIsSignup(!isSignup)}>
+                    {isSignup ? 'Already a member? Login' : 'New user? Signup here'}
+                </a>
+            </div>
+        </div>
+    )
+}
